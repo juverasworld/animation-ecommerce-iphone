e6eb30d83d7f1a592a56e5976370ca6ef3f6e826
Vee <ifebuchejulia@gmail.com>
2024-02-10 13:43:54 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import DisplaySection from "./components/DisplaySection";
+import Jumbotron from "./components/Jumbotron";
+import Navbar from "./components/Navbar";
+import SoundSection from "./components/SoundSection";
+function App() {
+
+  return (
+    <div className="App">
+    <Navbar/>
+    <Jumbotron/>
+    <SoundSection/>
+    <DisplaySection/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/DisplaySection.jsx
@@ -0,0 +1,12 @@
+
+
+
+export default function DisplaySection(){
+    return(
+        <>
+        <div className="">
+            
+        </div>
+        </>
+    )
+}
